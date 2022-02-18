9fbb07e739734781e22dbf6fe925c86ba2d8a501
Thomas Siegfried <thomas.siegfried@outlook.com>
2022-02-17 17:40:31 -0700

    fix for pureIsolate

--- spec/test_spec.ts
+++ spec/test_spec.ts
@@ -1,165 +1,190 @@
-import {Person,LoginModel,LoginService, LoginValidator, ClassIDontWantToCreateInMyTest} from './sampleTypes';
-import {AutoMocker} from '../src/index'; 
+import {
+  Person,
+  LoginModel,
+  LoginService,
+  LoginValidator,
+  ClassIDontWantToCreateInMyTest,
+} from "./sampleTypes";
+import { AutoMocker } from "../src/index";
 
-describe('AutoMocker',()=>{
-    var mkr:AutoMocker;
-    beforeEach(()=>{
-        mkr=new AutoMocker();
-    });
-    it('Can automock properties of a type',()=>{
-        mkr.AutoProp(Person);
-        var person = mkr.Resolve<Person>(Person);
-        person.firstname='FirstName';
-        person.lastname='LastName';
-        expect(person.firstname).toBe('FirstName'); //properties can set/get
-        expect(person.lastname).toBe('LastName');
-        expect(person.fullname).toBeUndefined();    //any unset property is undefined
-        
-    });
+describe("AutoMocker", () => {
+  var mkr: AutoMocker;
+  beforeEach(() => {
+    mkr = new AutoMocker();
+  });
+  it("Can automock properties of a type", () => {
+    mkr.AutoProp(Person);
+    var person = mkr.Resolve<Person>(Person);
+    person.firstname = "FirstName";
+    person.lastname = "LastName";
+    expect(person.firstname).toBe("FirstName"); //properties can set/get
+    expect(person.lastname).toBe("LastName");
+    expect(person.fullname).toBeUndefined(); //any unset property is undefined
+  });
 
-    it('can stub all methods of a type, causing them to return null',()=>{
-        mkr.Stub(LoginService);
-        let ls = mkr.Resolve<LoginService>(LoginService);
-        var res = ls.Login('','');  
-        expect(res).toBeNull();             
-    });
+  it("can stub all methods of a type, causing them to return null", () => {
+    mkr.Stub(LoginService);
+    let ls = mkr.Resolve<LoginService>(LoginService);
+    var res = ls.Login("", "");
+    expect(res).toBeNull();
+  });
 
-    it('can override specific stubbed properties',()=>{
-        mkr.Stub(LoginService);
-        var logoutSpy =mkr.Mock(LoginService,'Logout').and.returnValue(true);
-        let ls = mkr.Resolve<LoginService>(LoginService);
-        var res = ls.Login('','');
-        expect(res).toBeNull();
-        expect(ls.Logout()).toBeTrue();     //
-        expect(logoutSpy).toHaveBeenCalled();
-    });
+  it("can override specific stubbed properties", () => {
+    mkr.Stub(LoginService);
+    var logoutSpy = mkr.Mock(LoginService, "Logout").and.returnValue(true);
+    let ls = mkr.Resolve<LoginService>(LoginService);
+    var res = ls.Login("", "");
+    expect(res).toBeNull();
+    expect(ls.Logout()).toBeTrue(); //
+    expect(logoutSpy).toHaveBeenCalled();
+  });
 
-    it('Can Isolate a specific type, stubbing all others',()=>{
-        mkr.Isolate(LoginModel);    
-        var vm = mkr.Resolve<LoginModel>(LoginModel);
-        vm.username='test';
-        vm.password='test';
-        expect(vm.IsValid()).toBe(true);    //isolated class executes code normally
-        expect(vm.Submit()).toBeNull(); //stub service will return null;
-    });
+  it("Can Isolate a specific type, stubbing all others", () => {
+    mkr.Isolate(LoginModel);
+    var vm = mkr.Resolve<LoginModel>(LoginModel);
+    vm.username = "test";
+    vm.password = "test";
+    expect(vm.IsValid()).toBe(true); //isolated class executes code normally
+    expect(vm.Submit()).toBeNull(); //stub service will return null;
+  });
 
-    it('Can Isolate multiple types',()=>{
-        mkr.Isolate([LoginModel,LoginValidator]);
-        var vm = mkr.Resolve<LoginModel>(LoginModel);
-        expect(vm.SubmitWithExternalValidation()).toBeFalse(); //actual validator told us not to login
-        vm.username=vm.password='test'; //set username and password
-        expect(vm.SubmitWithExternalValidation()).toBe(null);   //calls through to login service, which is a stub
-    });
+  it("Can Isolate multiple types", () => {
+    mkr.Isolate([LoginModel, LoginValidator]);
+    var vm = mkr.Resolve<LoginModel>(LoginModel);
+    expect(vm.SubmitWithExternalValidation()).toBeFalse(); //actual validator told us not to login
+    vm.username = vm.password = "test"; //set username and password
+    expect(vm.SubmitWithExternalValidation()).toBe(null); //calls through to login service, which is a stub
+  });
 
-    it('Can create fluent style typeMocker',()=>{
-        var spy =mkr.Type<LoginService>(LoginService).Stub(true).Mock('Login').and.returnValue(true);
-        var vm =mkr.Resolve<LoginModel>(LoginModel);
-        vm.username='user';
-        vm.password='pass';
-        expect(vm.Submit()).toBeTrue();
-        expect(spy).toHaveBeenCalled();
-    });
+  it("Can create fluent style typeMocker", () => {
+    var spy = mkr
+      .Type<LoginService>(LoginService)
+      .Stub(true)
+      .Mock("Login")
+      .and.returnValue(true);
+    var vm = mkr.Resolve<LoginModel>(LoginModel);
+    vm.username = "user";
+    vm.password = "pass";
+    expect(vm.Submit()).toBeTrue();
+    expect(spy).toHaveBeenCalled();
+  });
 
-    it('Can mock methods using typed functions',()=>{
-        var mkLogin = mkr.Type<LoginService>(LoginService);
-        mkLogin.Stub(true);
-        var spyLogin = mkLogin.Mock(c=>c.Login).and.returnValue(true);     //login returns true
-        var vm =mkr.Resolve<LoginModel>(LoginModel);
-        vm.username='user';
-        vm.password='pass';
-        expect(vm.Submit()).toBeTrue();         //returns value from mock
-        expect(spyLogin).toHaveBeenCalled();    //verify mock was called
-    });
+  it("Can mock methods using typed functions", () => {
+    var mkLogin = mkr.Type<LoginService>(LoginService);
+    mkLogin.Stub(true);
+    var spyLogin = mkLogin.Mock((c) => c.Login).and.returnValue(true); //login returns true
+    var vm = mkr.Resolve<LoginModel>(LoginModel);
+    vm.username = "user";
+    vm.password = "pass";
+    expect(vm.Submit()).toBeTrue(); //returns value from mock
+    expect(spyLogin).toHaveBeenCalled(); //verify mock was called
+  });
 
-    it('can stub a property default value using fluent conventions',()=>{
-        mkr.Type<LoginService>(LoginService).Get(c=>c.ServiceName).and.returnValue("Override");
-        var vm = mkr.Resolve<LoginModel>(LoginModel);
-        expect(vm.GetServiceName()).toBe("Override");
-    });
+  it("can stub a property default value using fluent conventions", () => {
+    mkr
+      .Type<LoginService>(LoginService)
+      .Get((c) => c.ServiceName)
+      .and.returnValue("Override");
+    var vm = mkr.Resolve<LoginModel>(LoginModel);
+    expect(vm.GetServiceName()).toBe("Override");
+  });
 
-    it('can mock a property and validate a property get',()=>{
-        var pMock = mkr.Type<Person>(Person);
-        var spyFName = pMock.Get(p=>p.firstname).and.returnValue('Setup');
+  it("can mock a property and validate a property get", () => {
+    var pMock = mkr.Type<Person>(Person);
+    var spyFName = pMock.Get((p) => p.firstname).and.returnValue("Setup");
 
-        var person = mkr.Resolve<Person>(Person);   //resolve the mock
-        expect(person.firstname).toBe("Setup");
-        expect(spyFName).toHaveBeenCalled();
+    var person = mkr.Resolve<Person>(Person); //resolve the mock
+    expect(person.firstname).toBe("Setup");
+    expect(spyFName).toHaveBeenCalled();
+  });
 
-    });
+  it("can validate a property set", () => {
+    var pMock = mkr.Type<Person>(Person);
+    var spySetter = pMock.Set((p) => p.firstname);
 
-    it('can validate a property set',()=>{
-        var pMock = mkr.Type<Person>(Person);
-        var spySetter = pMock.Set(p=>p.firstname);
+    var person = mkr.Resolve<Person>(Person); //resolve the mock
+    person.firstname = "...something new";
+    expect(spySetter).toHaveBeenCalled();
+  });
 
-        var person = mkr.Resolve<Person>(Person);   //resolve the mock
-        person.firstname="...something new";
-        expect(spySetter).toHaveBeenCalled();
-    });
+  it("returns null from all properties if not automocked or spied on", () => {
+    var pMock = mkr.Type<Person>(Person);
+    pMock.Stub();
+    var person = mkr.Resolve<Person>(Person); //resolve the mock
+    person.firstname = "...something new";
+    expect(person.firstname).toBeFalsy();
+  });
 
-    it('returns null from all properties if not automocked or spied on',()=>{
-        var pMock = mkr.Type<Person>(Person);
-        pMock.Stub();
-        var person = mkr.Resolve<Person>(Person);   //resolve the mock
-        person.firstname="...something new";
-        expect(person.firstname).toBeFalsy();
-    });
+  it("calls mocked methods with correct values so that we can validate the call correctly", () => {
+    var pMock = mkr.TypeT(LoginService);
+    var spy = pMock.Mock((p) => p.Login).and.returnValue(true);
+    var svc = mkr.ResolveT(LoginService);
+    expect(svc.Login("test", "method")).toBeTrue();
+    expect(spy).toHaveBeenCalledWith("test", "method");
+  });
 
-    it('calls mocked methods with correct values so that we can validate the call correctly',()=>{
-        var pMock = mkr.TypeT(LoginService);
-        var spy = pMock.Mock(p=>p.Login).and.returnValue(true);
-        var svc = mkr.ResolveT(LoginService);
-        expect(svc.Login('test','method')).toBeTrue();
-        expect(spy).toHaveBeenCalledWith('test','method');
-    });
+  it("can infer constructor types using the TypeT method", () => {
+    var pMock = mkr.TypeT(Person);
+    pMock.Stub();
+    pMock.Get((p) => p.firstname).and.returnValue("test");
+  });
 
-    it('can infer constructor types using the TypeT method',()=>{
-        var pMock=mkr.TypeT(Person);
-        pMock.Stub();
-        pMock.Get(p=>p.firstname).and.returnValue('test');
-    })
+  it("can infer constructor types of constructors with parameters", () => {
+    var pMock = mkr.TypeT(LoginModel);
+    pMock.Stub();
+    pMock.Mock((p) => p.IsValid).and.returnValue(true);
+    mkr.ResolveT(LoginModel);
+  });
 
-    it('can infer constructor types of constructors with parameters',()=>{
-        var pMock=mkr.TypeT(LoginModel);
-        pMock.Stub();
-        pMock.Mock(p=>p.IsValid).and.returnValue(true);
-        mkr.ResolveT(LoginModel);
-    });
+  it("can create pureProxies with mocked functions without creating the object", () => {
+    var pMock = mkr.TypeT(ClassIDontWantToCreateInMyTest);
+    pMock
+      .PureProxy(false)
+      .Mock((t) => t.ThrowError)
+      .and.returnValue(1);
+    var obj = mkr.ResolveT(ClassIDontWantToCreateInMyTest);
+    expect(obj.ThrowError()).toBe(1);
+  });
 
-    it('can create pureProxies with mocked functions without creating the object',()=>{
-        var pMock=mkr.TypeT(ClassIDontWantToCreateInMyTest);
-        pMock.PureProxy(false).Mock(t=>t.ThrowError).and.returnValue(1);
-        var obj = mkr.ResolveT(ClassIDontWantToCreateInMyTest);
-        expect(obj.ThrowError()).toBe(1);
-    });
+  it("can infer types from mocked props and methods", () => {
+    //this is not really a test, just a reminder
+    var lmMock = mkr.TypeT(LoginModel);
+    lmMock.MockT((m) => m.Submit).and.returnValue(true); //cannot specifiy a non-boolean
+  });
 
-    it('can infer types from mocked props and methods',()=>{
-        //this is not really a test, just a reminder
-        var lmMock = mkr.TypeT(LoginModel);
-        lmMock.MockT((m)=>m.Submit).and.returnValue(true); //cannot specifiy a non-boolean
-    })
+  it("can infer typed spies for properties", () => {
+    var pMock = mkr.TypeT(LoginModel);
+    pMock.GetT((m) => m.username).and.returnValue("test");
+  });
 
-    it('can infer typed spies for properties',()=>{
-        var pMock = mkr.TypeT(LoginModel);
-        pMock.GetT(m=>m.username).and.returnValue("test");
-    })
+  it("can isolate a type so that all non-isolated types are pure proxies", () => {
+    mkr.PureIsolate(LoginModel);
+    //this should be a pure mock
+    //i can create it
+    var tst = mkr.ResolveT(ClassIDontWantToCreateInMyTest);
+    //properties return null
+    expect(tst.ThrowError()).toBeNull();
+  });
 
-    it('can isolate a type so that all non-isolated types are pure proxies',()=>{
-        mkr.PureIsolate(LoginModel);
-        //this should be a pure mock
-        //i can create it
-        var tst = mkr.ResolveT(ClassIDontWantToCreateInMyTest);
-        //properties return null
-        expect(tst.ThrowError()).toBeNull();
-    });
+  it("returns non proxied instances of isolated types", () => {
+    mkr.PureIsolate(LoginModel);
+    var login = mkr.ResolveT(LoginModel);
+    login.username = "test";
+    login.password = "test";
+    expect(login.username).toBe("test");
+    expect(login.IsValid()).toBeTrue();
+  });
 
-    it('pure isolate proxies can still be mocked',()=>{
-        mkr.PureIsolate(LoginModel);
-        mkr.TypeT(ClassIDontWantToCreateInMyTest).MockT(c=>c.DummyMethod).and.returnValue(5);
-        //this should be a pure mock
-        //i can create it
-        var tst = mkr.ResolveT(ClassIDontWantToCreateInMyTest);
-        //methods return null
-        expect(tst.DummyMethod()).toBe(5);
-    });
-
+  it("pure isolate proxies can still be mocked", () => {
+    mkr.PureIsolate(LoginModel);
+    mkr
+      .TypeT(ClassIDontWantToCreateInMyTest)
+      .MockT((c) => c.DummyMethod)
+      .and.returnValue(5);
+    //this should be a pure mock
+    //i can create it
+    var tst = mkr.ResolveT(ClassIDontWantToCreateInMyTest);
+    //methods return null
+    expect(tst.DummyMethod()).toBe(5);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,186 +1,186 @@
-import {Injector} from '@thomas-siegfried/jsi';
-const WILDCARD:string ='*';
-export class AutoMocker{
-    constructor(public injector:Injector=null){
-        if(!this.injector){
-            this.injector=new Injector();
+import { Injector } from "@thomas-siegfried/jsi";
+const WILDCARD: string = "*";
+export class AutoMocker {
+  constructor(public injector: Injector = null) {
+    if (!this.injector) {
+      this.injector = new Injector();
+    }
+  }
+
+  Resolve<T>(key: any) {
+    return this.injector.Resolve<T>(key);
+  }
+
+  ResolveT<T>(key: Constructor<T>) {
+    return this.injector.Resolve<T>(key);
+  }
+  //configure a type for auto prop behavior
+  //we can set any property and get back the value we set
+  AutoProp(key: any) {
+    var pxy = this.injector.Proxy(key);
+    var autoObjMap = new Map();
+    const getAutoObj = (obj) => {
+      if (!autoObjMap.has(obj)) {
+        autoObjMap.set(obj, {});
+      }
+      return autoObjMap.get(obj);
+    };
+    pxy.get(WILDCARD).instead((obj, prop) => {
+      return getAutoObj(obj)[prop];
+    });
+    pxy.set(WILDCARD).instead((obj, prop, val) => {
+      getAutoObj(obj)[prop] = val;
+    });
+  }
+
+  //All of the objects methods and props will return null
+  Stub(key: any, autoProp: boolean = false) {
+    var pxy = this.injector.Proxy(key);
+    pxy.fn(WILDCARD).instead(() => null);
+    if (autoProp) {
+      //setup automatic properties
+      this.AutoProp(key);
+    } else {
+      //setup stubbed properties
+      pxy.get(WILDCARD).instead(() => null);
+      pxy.set(WILDCARD).instead(() => {});
+    }
+  }
+
+  //specific method will be a spy, returned to caller
+  Mock(key: any, Method: string): jasmine.Spy {
+    var spy = jasmine.createSpy();
+    var pxy = this.injector.Proxy(key);
+    pxy.fn(Method).instead((obj, name, args) => spy(...args));
+    return spy;
+  }
+
+  Type<T>(key: any): TypeMocker<T> {
+    return new TypeMocker<T>(key, this);
+  }
+
+  TypeT<T>(key: Constructor<T>): TypeMocker<T> {
+    return new TypeMocker<T>(key, this);
+  }
+
+  //override the get result of a specific property
+  Get(key: any, prop: string): jasmine.Spy {
+    var spy = jasmine.createSpy();
+    var pxy = this.injector.Proxy(key);
+    pxy.get(prop).instead(() => spy());
+    return spy;
+  }
+
+  Set(key: any, prop: string): jasmine.Spy {
+    var spy = jasmine.createSpy();
+    var pxy = this.injector.Proxy(key);
+    pxy.set(prop).instead((t, p, v) => spy(v));
+    return spy;
+  }
+
+  private isolatedTypes: any[];
+  PureIsolate(key: any | any[]) {
+    if (!Array.isArray(key)) {
+      key = [key];
+    }
+    if (!this.isolatedTypes) {
+      this.isolatedTypes = [];
+      this.injector.callbacks.Resolve = (key, stack) => {
+        //anything that gets requested is a pure proxy unless it is on the isolate list
+        if (this.isolatedTypes.indexOf(key) == -1 && key.prototype) {
+          this.PureProxy(key, true);
         }
+      };
     }
+    this.isolatedTypes.push(...key);
+  }
+  Isolate(key: any | any[]) {
+    if (!Array.isArray(key)) {
+      key = [key];
+    }
+    //proxy all isolated types with no config (effectively not proxied)
+    key.forEach((k) => this.injector.Proxy(k));
 
-    Resolve<T>(key:any){
-        return this.injector.Resolve<T>(key);
+    this.Stub(WILDCARD, true); //all methods return null
+  }
+  //register a key to a factory returning dummy object based on key's prototype
+  //this only works if the key is a constructor
+  PureProxy(key: any, autoProp: boolean) {
+    if (!key.prototype) {
+      throw new Error();
     }
-
-    ResolveT<T>(key:Constructor<T>){
-        return this.injector.Resolve<T>(key);
-    }
-    //configure a type for auto prop behavior
-    //we can set any property and get back the value we set
-    AutoProp(key:any){
-        var pxy =this.injector.Proxy(key);
-        var autoObjMap = new Map();
-        const getAutoObj = (obj)=>{
-            if(!autoObjMap.has(obj)){
-                autoObjMap.set(obj,{});
-            }
-            return autoObjMap.get(obj);
-        }
-        pxy.get(WILDCARD).instead((obj,prop)=>{
-            return getAutoObj(obj)[prop];
-        });
-        pxy.set(WILDCARD).instead((obj,prop,val)=>{
-            getAutoObj(obj)[prop]=val;
-        });
-    }
-
-    //All of the objects methods and props will return null
-    Stub(key:any,autoProp:boolean=false){ 
-        var pxy = this.injector.Proxy(key);
-        pxy.fn(WILDCARD).instead(()=>null);
-        if(autoProp){
-            //setup automatic properties
-            this.AutoProp(key);
-        } else{
-            //setup stubbed properties
-            pxy.get(WILDCARD).instead(()=>null);
-            pxy.set(WILDCARD).instead(()=>{});
-        }
-    }
-
-    //specific method will be a spy, returned to caller
-    Mock(key:any,Method:string):jasmine.Spy{
-        var spy = jasmine.createSpy();
-        var pxy = this.injector.Proxy(key);
-        pxy.fn(Method).instead((obj,name,args)=>spy(...args))
-        return spy;
-    }
-
-
-    Type<T>(key:any):TypeMocker<T>{
-        return new TypeMocker<T>(key,this);
-    }
-    
-    TypeT<T>(key:Constructor<T>):TypeMocker<T>{
-        return new TypeMocker<T>(key,this);
-    }
-    
-    //override the get result of a specific property
-    Get(key:any,prop:string):jasmine.Spy{
-        var spy = jasmine.createSpy();
-        var pxy = this.injector.Proxy(key);
-        pxy.get(prop).instead(()=>spy());
-        return spy;
-    }
-
-    Set(key:any,prop:string):jasmine.Spy{
-        var spy = jasmine.createSpy();
-        var pxy = this.injector.Proxy(key);
-        pxy.set(prop).instead((t,p,v)=>spy(v));
-        return spy;
-    }
-
-    private isolatedTypes:any[];
-    PureIsolate(key:any|any[]){
-        if(!Array.isArray(key)){
-            key=[key];
-        }
-        if(!this.isolatedTypes){
-            this.isolatedTypes=[];
-            this.injector.callbacks.Resolve=(key,stack)=>{
-                //anything that gets requested is a pure proxy unless it is on the isolate list
-                if(this.isolatedTypes.indexOf(key)==-1 && key.prototype){
-                    this.PureProxy(key,true);
-                }
-            };
-        }
-        this.isolatedTypes.push();
-    }
-    Isolate(key:any|any[]){
-        if(!Array.isArray(key)){
-            key=[key];
-        }
-        //proxy all isolated types with no config (effectively not proxied)
-       key.forEach(k=>this.injector.Proxy(k));
-
-        this.Stub(WILDCARD,true);     //all methods return null
-    }
-    //register a key to a factory returning dummy object based on key's prototype
-    //this only works if the key is a constructor 
-    PureProxy(key:any,autoProp:boolean){
-        if(!key.prototype){
-            throw new Error()
-        }
-        this.injector.RegisterOptions({Key:key,Factory:()=>Object.create(key.prototype)});
-        this.Stub(key,autoProp);
-    }
-
+    this.injector.RegisterOptions({
+      Key: key,
+      Factory: () => Object.create(key.prototype),
+    });
+    this.Stub(key, autoProp);
+  }
 }
 
-export interface Func<T>{
-    (t:T):any;
+export interface Func<T> {
+  (t: T): any;
 }
-export interface Func2<T,K>{
-    (t:T):K;
-}
-
-export class TypeMocker<T>{
-    constructor(private key:any,private mocker:AutoMocker){
-    }
-
-    //All of the objects methods will return null;
-    Stub(autoProp:boolean=false){
-        this.mocker.Stub(this.key,autoProp);
-        return this;
-    }
-    //mock a specific method, returns the spy, and thus breaks the fluency
-    Mock(method:string|Func<T>):jasmine.Spy{
-        return this.mocker.Mock(this.key,this.GetMember(method));
-     
-    }
-
-    MockT<K>(method:(t:T)=>()=>K):jasmine.Spy<()=>K>{
-        var fn =this.parseFunction(method);
-        return this.mocker.Mock(this.key,this.GetMember(fn)) as jasmine.Spy<()=>K>; 
-    }
-
-    Get(prop:string|Func<T>):jasmine.Spy{
-        return this.mocker.Get(this.key,this.GetMember(prop));
-    }
-
-    GetT<K>(prop:(t:T)=>K):jasmine.Spy<()=>K>{
-        var fn =this.parseFunction(prop);
-        return this.mocker.Get(this.key,this.GetMember(fn)) as jasmine.Spy<()=>K>; 
-    }
-
-    Set(prop:string|Func<T>):jasmine.Spy{
-        return this.mocker.Set(this.key,this.GetMember(prop));
-    }
-
-    /*Do not create the object, return an instance based on prototype and stub all methods*/
-    PureProxy(autoProp:boolean=false){
-        this.mocker.PureProxy(this.key,autoProp);
-        return this;
-    }
-
-    private GetMember(member:string|Func<T>){
-        if(typeof member=='string')
-            return member;
-        else
-            return this.parseFunction(member);
-    }
-
-    //brute force attempt to pull a member from a function simillar to Expression in C#
-    //expects functions in the form of d=>d.Xyz or (d)=>d.abc etc
-    private parseFunction(fn:(i:any)=>any):string{
-        let txt = fn.toString();
-        let prm =txt.substring(0,txt.indexOf("=>")).replace(/[(|)]/g,'').trim();
-        let rest = txt.substring(txt.indexOf('=>')+2).trim();
-        let member= rest.substring(prm.length+1);
-        return member;
-    }
-       
+export interface Func2<T, K> {
+  (t: T): K;
 }
 
-interface Constructor<T>{
-    new (...args:any[]):T;
+export class TypeMocker<T> {
+  constructor(private key: any, private mocker: AutoMocker) {}
+
+  //All of the objects methods will return null;
+  Stub(autoProp: boolean = false) {
+    this.mocker.Stub(this.key, autoProp);
+    return this;
+  }
+  //mock a specific method, returns the spy, and thus breaks the fluency
+  Mock(method: string | Func<T>): jasmine.Spy {
+    return this.mocker.Mock(this.key, this.GetMember(method));
+  }
+
+  MockT<K>(method: (t: T) => () => K): jasmine.Spy<() => K> {
+    var fn = this.parseFunction(method);
+    return this.mocker.Mock(this.key, this.GetMember(fn)) as jasmine.Spy<
+      () => K
+    >;
+  }
+
+  Get(prop: string | Func<T>): jasmine.Spy {
+    return this.mocker.Get(this.key, this.GetMember(prop));
+  }
+
+  GetT<K>(prop: (t: T) => K): jasmine.Spy<() => K> {
+    var fn = this.parseFunction(prop);
+    return this.mocker.Get(this.key, this.GetMember(fn)) as jasmine.Spy<
+      () => K
+    >;
+  }
+
+  Set(prop: string | Func<T>): jasmine.Spy {
+    return this.mocker.Set(this.key, this.GetMember(prop));
+  }
+
+  /*Do not create the object, return an instance based on prototype and stub all methods*/
+  PureProxy(autoProp: boolean = false) {
+    this.mocker.PureProxy(this.key, autoProp);
+    return this;
+  }
+
+  private GetMember(member: string | Func<T>) {
+    if (typeof member == "string") return member;
+    else return this.parseFunction(member);
+  }
+
+  //brute force attempt to pull a member from a function simillar to Expression in C#
+  //expects functions in the form of d=>d.Xyz or (d)=>d.abc etc
+  private parseFunction(fn: (i: any) => any): string {
+    let txt = fn.toString();
+    let prm = txt.substring(0, txt.indexOf("=>")).replace(/[(|)]/g, "").trim();
+    let rest = txt.substring(txt.indexOf("=>") + 2).trim();
+    let member = rest.substring(prm.length + 1);
+    return member;
+  }
+}
+
+interface Constructor<T> {
+  new (...args: any[]): T;
 }
